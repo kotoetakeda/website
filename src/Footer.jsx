00f0e2ff413d5896370aa51bcea8e56dd712b763
kotoetakeda <kotoetakeda0116@gmail.com>
2024-07-20 13:33:57 -0700

    cleaned up files + imports

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { useTheme } from "@chakra-ui/react"; 
-import { Box, Flex, HStack, Divider, Text, Image, Icon } from "@chakra-ui/react";
-import { H1, H2, L1, Body } from "./Custom";
-import { customTheme } from "./index";
+import { Flex, Text, Icon } from "@chakra-ui/react";
 import { MdEmail } from "react-icons/md";
 import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
 
@@ -12,25 +10,25 @@ const Footer = () => {
   const colors = theme.colors;
   return (
     <>
-    <Flex p={'0.5rem 10rem'} w={'100%'} bg={colors.white} align={'baseline'}>
+    <Flex p={'0.5rem 10rem'} w={'100%'} bg={colors.white} align={'baseline'} position={'fixed'} bottom={'0'}>
         <Flex align={'center'}  justify={'space-between'} w={'50%'}>  
           <Text fontSize={'lg'} fontWeight={'900'}>KOTOE TAKEDA</Text>
           <Flex justify={'space-between'} align={'center'} w={'70%'}>
-            <a href="mailto:ktakeda3@gatech.edu" target="_blank">
+            <a href="mailto:ktakeda3@gatech.edu" target="_blank" rel={'noreferrer'}>
               <Flex gap="0.5rem" align={'center'}>
                 <Icon as={MdEmail} boxSize={4}/>
                 <Text fontSize={'sm'}>ktakeda3@gatech.edu</Text>
               </Flex>
             </a>
 
-            <a href="https://www.linkedin.com/in/kotoetakeda/" target="_blank">
+            <a href="https://www.linkedin.com/in/kotoetakeda/" target="_blank" rel={'noreferrer'}>
               <Flex gap="0.5rem" align={'center'}>
                 <Icon as={FaLinkedin} boxSize={4}/>
                 <Text fontSize={'sm'}>kotoetakeda</Text>
               </Flex>
             </a>
 
-            <a href="https://github.com/kotoetakeda" target="_blank">
+            <a href="https://github.com/kotoetakeda" target="_blank" rel={'noreferrer'}>
               <Flex gap="0.5rem" align={'center'}>
                 <Icon as={FaGithubSquare} boxSize={4}/>
                 <Text fontSize={'sm'}>kotoetakeda</Text>
