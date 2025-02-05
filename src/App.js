7e55d23f3e912a13f4a8843c76f04161dc6bd8e1
kotoetakeda <kotoetakeda0116@gmail.com>
2025-02-05 15:19:34 -0500

    fixed tailwind and style not showing up issue

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import SAQ from './SAQ';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/website" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/saq" element={<SAQ />} />
-      </Routes>
-    </Router>
+    <div className="px-5 sm:px-5 md:px-10 lg:px-15 xl:px-20 py-5 sm:py-5 md:py-10 lg:py-10 xl:py-10 w-screen bg-stone-200 text-stone-950">
+      <Router>
+        <Routes>
+          <Route path="/website" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/saq" element={<SAQ />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
